6f61fcdb07c696c15d3338f1306f3b03529d7c51
Mark <m5145692923@gmail.com>
2021-07-12 12:01:32 -0400

    user auth.

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -28,5 +28,34 @@ router.post('/login',async (req,res)=>{
         res.status(500).json({message:err.message})
     }
 })
+// user register
+router.post("/signup",async (req,res)=>{
+    const exist = await userModel.findOne({username:req.body.username})
+    if (exist != null) {
+        res.status(400).json({message:"username already exist."});
+        return;
+    }
+    try{
+        req.body.password = await bcrypt.hash(req.body.password,10)
+        const user = new userModel(req.body)
+        const newUser = await user.save()
+        res.json(newUser);
+    }catch(err){
+        res.json({message:err.message});
+    }
+})
+
+router.post("/validateToken",(req,res)=>{
+    const accessToken = req.headers.authorization;
+    const token = accessToken && accessToken.split(" ")[1];
+    if (token == null) return res.status(403).json({message:"access denied."})
+    jwt.verify(token,process.env.JWT_SECRET,(err,user)=>{
+        if (err) {
+            return res.status(403).json({message:"Token invalid"})
+        }
+        return res.json(user)
+    })
+
+})
 
 module.exports = router
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -33,22 +33,6 @@ router.get("/:id", userAuth,async (req,res)=>{
     }
 })
 
-router.post("/",async (req,res)=>{
-    const exist = await userModel.findOne({username:req.body.username})
-    if (exist != null) {
-        res.status(400).json({message:"username already exist."});
-        return;
-    }
-    try{
-        req.body.password = await bcrypt.hash(req.body.password,10)
-        const user = new userModel(req.body)
-        const newUser = await user.save()
-        res.json(newUser);
-    }catch(err){
-        res.json({message:err.message});
-    }
-})
-
 router.patch("/:id",userAuth, async (req,res)=>{
 
     try{
@@ -83,18 +67,6 @@ router.delete("/:id",userAuth ,async (req,res)=>{
     }
 })
 
-router.post("/validateToken",(req,res)=>{
-    const accessToken = req.headers.authorization;
-    const token = accessToken && accessToken.split(" ")[1];
-    if (token == null) return res.status(403).json({message:"access denied."})
-    jwt.verify(token,process.env.JWT_SECRET,(err,user)=>{
-        if (err) {
-            return res.status(403).json({message:"Token invalid"})
-        }
-        return res.json(user)
-    })
-
-})
 
 async function userAuth(req,res,next){
     const headerToken = req.headers.authorization;
@@ -114,5 +86,7 @@ async function userAuth(req,res,next){
     }
 }
 
+
+router.use(userAuth)
 
 module.exports = router;
